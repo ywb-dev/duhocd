949acb0d0764f7183d6c1445f4cab6c08d897d11
YeowubieInteractions <142081054+yeowubie-interactions@users.noreply.github.com>
2023-12-19 09:01:47 +0700

    update api url

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default defineNuxtPlugin(nuxtApp => {
-    axios.defaults.baseURL = 'http://127.0.0.1:8000';
+    axios.defaults.baseURL = 'http://api.duhocd.com';
     axios.defaults.withCredentials = false;
     // axios.defaults.proxyHeaders = false;
 

5e6d75afc45af50e019adea008cd30275fe99a60
YeowubieInteractions <142081054+yeowubie-interactions@users.noreply.github.com>
2023-12-21 18:43:04 +0700

    register and login ui customer

---
+++ components/Sections/RegisterPart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full bg-grey-500">
+        <div class="container">
+            <div class="mt-10 py-6 md:pt-20 md:pb-10 flex flex-col md:flex-row justify-between gap-6">
+                <div class="md:pr-12 w-full he-full">
+                    <h3
+                        class="font-black text-black -tracking-[3px] text-xl md:text-3xl lg:text-[42px] leading-[48px] m-0 mb-2">
+                        한국 유학생을 모집 중 이세요?</h3>
+                    <p
+                        class="text-sm md:text-base lg:text-xl max-w-md text-black tracking-tighter font-medium leading-7 mb-0">
+                        <a class="underline" href="https://www.duhocd.com/">DUHOCD.com</a> 에서 유학원 회원으로 가입하시고, 베트남 학생들에게 귀사의
+                        상품을 홍보하세요.</p>
+                </div>
+                <button class="btn-fe box-regis">Register</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>

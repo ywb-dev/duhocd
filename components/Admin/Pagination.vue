347500b8f4d7c19a1b350ec3a0f64ecab479896f
YeowubieInteractions <142081054+yeowubie-interactions@users.noreply.github.com>
2023-12-27 10:23:43 +0700

    update admin

--- components/Admin/Pagination.vue
+++ components/Admin/Pagination.vue
@@ -1,23 +1,23 @@
 <template>
   <nav class="flex justify-center w-full items-center py-8 px-6" aria-label="Page navigation">
     <ul class="pagination">
-      <li class="page-item prev-first" :class="{ 'disabled': pagination.current_page === 1 }">
+      <li class="page-item prev-first" :class="{ 'disabled': pagination?.current_page === 1 }">
         <a class="page-link" @click.prevent="changePage(1)" href="#"><i class="pi pi-angle-double-left"></i></a>
       </li>
-      <li class="page-item prev" :class="{ 'disabled': pagination.current_page === 1 }">
-        <a class="page-link" @click.prevent="changePage(pagination.current_page - 1)" href="#"><i
+      <li class="page-item prev" :class="{ 'disabled': pagination?.current_page === 1 }">
+        <a class="page-link" @click.prevent="changePage(pagination?.current_page - 1)" href="#"><i
             class="pi pi-chevron-left"></i></a>
       </li>
-      <li v-for="page in pages" :key="page" :class="{ 'active': page === pagination.current_page }"
+      <li v-for="page in pages" :key="page" :class="{ 'active': page === pagination?.current_page }"
         class="page-item page-number">
         <a class="page-link" @click.prevent="changePage(page)" href="#">{{ page }}</a>
       </li>
-      <li class="page-item next" :class="{ 'disabled': pagination.current_page === pagination.last_page }">
-        <a class="page-link" @click.prevent="changePage(pagination.current_page + 1)" href="#"><i
+      <li class="page-item next" :class="{ 'disabled': pagination?.current_page === pagination?.last_page }">
+        <a class="page-link" @click.prevent="changePage(pagination?.current_page + 1)" href="#"><i
             class="pi pi-chevron-right"></i></a>
       </li>
-      <li class="page-item prev-last" :class="{ 'disabled': pagination.current_page === pagination.last_page }">
-        <a class="page-link" @click.prevent="changePage(pagination.last_page)" href="#"><i
+      <li class="page-item prev-last" :class="{ 'disabled': pagination?.current_page === pagination?.last_page }">
+        <a class="page-link" @click.prevent="changePage(pagination?.last_page)" href="#"><i
             class="pi pi-angle-double-right"></i></a>
       </li>
     </ul>
@@ -35,7 +35,7 @@ export default {
   computed: {
     pages() {
       let pages = [];
-      for (let i = 1; i <= this.pagination.last_page; i++) {
+      for (let i = 1; i <= this.pagination?.last_page; i++) {
         pages.push(i);
       }
       return pages;
--- pages/admin/blogs/create.vue
+++ pages/admin/blogs/create.vue
@@ -102,103 +102,106 @@ const savePost = handleSubmit(async (values) => {
 })
 </script>
 <template>
-    <div class="relative w-full">
-        <div class="flex flex-wrap">
-            <PrimeToast />
-            <div class="w-full lg:bg-[#f8f9fa] rounded-xl p-0 lg:p-8">
-                <div class="flex items-center">
-                    <PrimeButton @click="$router.back()" icon="pi pi-arrow-left" severity="info" />
-                    <span class="text-base ml-4">Back to lists</span>
-                </div>
-                <div class="content__wrap flex flex-col lg:flex-row">
-                    <div class="flex-1 pb-20">
-                        <div class="flex flex-col">
-                            <h2 class="mt-10 mb-6">Add new posts</h2>
+    <client-only>
+        <div class="relative w-full">
+            <div class="flex flex-wrap">
+                <PrimeToast />
+                <div class="w-full lg:bg-[#f8f9fa] rounded-xl p-0 lg:p-8">
+                    <div class="flex items-center">
+                        <PrimeButton @click="$router.back()" icon="pi pi-arrow-left" severity="info" />
+                        <span class="text-base ml-4">Back to lists</span>
+                    </div>
+                    <div class="content__wrap flex flex-col lg:flex-row">
+                        <div class="flex-1 pb-20">
+                            <div class="flex flex-col">
+                                <h2 class="mt-10 mb-6">Add new posts</h2>
+                            </div>
+                            <div class="field-post flex flex-col mb-8">
+                                <label class="mb-2" for="title">Post Title</label>
+                                <span class="p-input-icon-left">
+                                    <i class="pi pi-align-right" />
+                                    <PrimeInputText id="title" :pt="{
+                                        root: { class: 'w-full' }
+                                    }" v-model="title" placeholder="Nhập title" :class="{ 'p-invalid' : titleError }" />
+                                </span>
+                                <small class="p-error mt-2">{{ titleError }}</small>
+                            </div>
+                            <div class="field-post flex flex-col mb-8">
+                                <label class="mb-2" for="title">Descriptions</label>
+                                <span class="p-input-icon-left">
+                                    <PrimeTextarea id="title" :pt="{
+                                        root: { class: 'w-full p-4' },
+                                        input: { class: 'w-full p-4' }
+                                    }" v-model="description" placeholder="Nhập mô tả" autoResize :class="{ 'p-invalid' : descriptionError }" rows="5" />
+                                </span>
+                                <small class="p-error mt-2">{{ descriptionError }}</small>
+                            </div>
+                            <div class="blog banner lg:p-6 bg-white rounded-xl">
+                                <h3 class="mb-2 mt-0">Summary</h3>
+                                <i>Thêm nôi dung của bài post ở đây để hiển thị ra Blog</i>
+                                <Editor class="mt-6" v-model="content" editorStyle="height: 500px" />
+                            </div>
+                            <small class="p-error">{{ contentError }}</small>
+                            <div class="action flex justify-end mt-8">
+                                <PrimeButton class="mr-6" @click="savePost" severity="info">Save</PrimeButton>
+                                <PrimeButton @click="" severity="help" text raised :pt="{
+                                    root: { class: 'border border-solid border-primary text-primary' },
+                                    label: { class: 'white' }
+                                }">Cancel</PrimeButton>
+                            </div>
                         </div>
-                        <div class="field-post flex flex-col mb-8">
-                            <label class="mb-2" for="title">Post Title</label>
-                            <span class="p-input-icon-left">
-                                <i class="pi pi-align-right" />
-                                <PrimeInputText id="title" :pt="{
-                                    root: { class: 'w-full' }
-                                }" v-model="title" placeholder="Nhập title" :class="{ 'p-invalid' : titleError }" />
-                            </span>
-                            <small class="p-error mt-2">{{ titleError }}</small>
-                        </div>
-                        <div class="field-post flex flex-col mb-8">
-                            <label class="mb-2" for="title">Descriptions</label>
-                            <span class="p-input-icon-left">
-                                <PrimeTextarea id="title" :pt="{
-                                    root: { class: 'w-full p-4' },
-                                    input: { class: 'w-full p-4' }
-                                }" v-model="description" placeholder="Nhập mô tả" autoResize :class="{ 'p-invalid' : descriptionError }" rows="5" />
-                            </span>
-                            <small class="p-error mt-2">{{ descriptionError }}</small>
-                        </div>
-                        <div class="blog banner lg:p-6 bg-white rounded-xl">
-                            <h3 class="mb-2 mt-0">Summary</h3>
-                            <i>Thêm nôi dung của bài post ở đây để hiển thị ra Blog</i>
-                            <Editor class="mt-6" v-model="content" editorStyle="height: 500px" />
-                        </div>
-                        <small class="p-error">{{ contentError }}</small>
-                        <div class="action flex justify-end mt-8">
-                            <PrimeButton class="mr-6" @click="savePost" severity="info">Save</PrimeButton>
-                            <PrimeButton @click="" severity="help" text raised :pt="{
-                                root: { class: 'border border-solid border-primary text-primary' },
-                                label: { class: 'white' }
-                            }">Cancel</PrimeButton>
+                        <div class="sidebar relative w-full lg:max-w-[340px] mt-6 lg:mt-0 ml-0 lg:ml-6">
+                            <div class="w-full rounded-xl p-8 flex flex-col bg-white mb-6">
+                                <h4 class="mt-0 text-base font-medium">Visibility</h4>
+                                <div v-for="action in actions" :key="action.key" class="flex align-items-center mb-4">
+                                    <PrimeRadioButton v-model="selectedAction" :inputId="action.key" name="dynamic"
+                                        :value="action.value" />
+                                    <label :for="action.key" class="ml-2 text-sm">{{ action.name }}</label>
+                                </div>
+                            </div>
+                            <div class="w-full rounded-xl p-8 flex flex-col bg-white mb-6">
+                                <PrimeDropdown v-model="categoryId" showClear :options="categories"  optionLabel="name"
+                                    placeholder="category" class="w-full " />
+                                <div class="w-full h-px bg-[#ced4da] my-2"></div>
+                                <div class="card relative">
+                                    <h4 class="mt-0 mb-4 text-base font-medium">Feature Image</h4>
+                                    <label for="avatar" class="mb-4">Banner</label>
+                                    <input type="file" accept="image/*" @change="onUpload" :maxFileSize="1000000">
+                                </div>
+                                <small class="p-error mt-2">{{ bannerError }}</small>
+                                <div class="mt-4">
+                                    <label for="tag" class="text-sm font-medium">Tags:</label>
+                                    <PrimeChips id="tag" class="w-full mt-2 " v-model="tags" separator="," :pt="{
+                                        container: { class: 'w-full' },
+                                        label: { class: 'text-white' },
+                                        removeTokenIcon: { class: 'text-white' },
+                                        token: { class: 'bg-primary' }
+                                    }" />
+                                    <small class="mt-2 block">Press Enter or ',' to add multiple tags</small>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="sidebar relative w-full lg:max-w-[340px] mt-6 lg:mt-0 ml-0 lg:ml-6">
-                        <div class="w-full rounded-xl p-8 flex flex-col bg-white mb-6">
-                            <h4 class="mt-0 text-base font-medium">Visibility</h4>
-                            <div v-for="action in actions" :key="action.key" class="flex align-items-center mb-4">
-                                <PrimeRadioButton v-model="selectedAction" :inputId="action.key" name="dynamic"
-                                    :value="action.value" />
-                                <label :for="action.key" class="ml-2 text-sm">{{ action.name }}</label>
-                            </div>
-                        </div>
-                        <div class="w-full rounded-xl p-8 flex flex-col bg-white mb-6">
-                            <PrimeDropdown v-model="categoryId" showClear :options="categories"  optionLabel="name"
-                                placeholder="category" class="w-full " />
-                            <div class="w-full h-px bg-[#ced4da] my-2"></div>
-                            <div class="card relative">
-                                <h4 class="mt-0 mb-4 text-base font-medium">Feature Image</h4>
-                                <label for="avatar" class="mb-4">Banner</label>
-                                <input type="file" accept="image/*" @change="onUpload" :maxFileSize="1000000">
-                            </div>
-                            <small class="p-error mt-2">{{ bannerError }}</small>
-                            <div class="mt-4">
-                                <label for="tag" class="text-sm font-medium">Tags:</label>
-                                <PrimeChips id="tag" class="w-full mt-2 " v-model="tags" separator="," :pt="{
-                                    container: { class: 'w-full' },
-                                    label: { class: 'text-white' },
-                                    removeTokenIcon: { class: 'text-white' },
-                                    token: { class: 'bg-primary' }
-                                }" />
-                                <small class="mt-2 block">Press Enter or ',' to add multiple tags</small>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+                <PrimeDialog v-model:visible="showModal" modal :style="{ width: '450px' }" header="Warning">
+                    <div class="flex align-items-center justify-content-center">
+                        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+                        <span>
+                            Bạn vừa có thay đổi chưa lưu. Bạn có chắc là muốn thoát không?</span>
+                    </div>
+                    <template #footer>
+                        <PrimeButton label="Discard" @click="leavePage" icon="pi pi-times" severity="info" outlined
+                            class="p-PrimeButton-text" :pt="{
+                                label: { class: 'font-semibold' }
+                            }" />
+                        <PrimeButton label="Save Post" icon="pi pi-check" @click="savePost" severity="info" outlined
+                            class="p-PrimeButton-text" :pt="{
+                                label: { class: 'font-semibold' }
+                            }" />
+                    </template>
+                </PrimeDialog>
             </div>
-            <PrimeDialog v-model:visible="showModal" modal :style="{ width: '450px' }" header="Warning">
-                <div class="flex align-items-center justify-content-center">
-                    <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-                    <span>
-                        Bạn vừa có thay đổi chưa lưu. Bạn có chắc là muốn thoát không?</span>
-                </div>
-                <template #footer>
-                    <PrimeButton label="Discard" @click="leavePage" icon="pi pi-times" severity="info" outlined
-                        class="p-PrimeButton-text" :pt="{
-                            label: { class: 'font-semibold' }
-                        }" />
-                    <PrimeButton label="Save Post" icon="pi pi-check" @click="savePost" severity="info" outlined
-                        class="p-PrimeButton-text" :pt="{
-                            label: { class: 'font-semibold' }
-                        }" />
-                </template>
-            </PrimeDialog>
         </div>
-    </div>
+        <CKEditor/>
+    </client-only>
 </template>
